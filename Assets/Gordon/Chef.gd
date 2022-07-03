6461f4f28efe0849f4f5b3a032f63cb3a212ae9a
Arthur Schevey <arthur.schevey@gmail.com>
2022-07-04 01:13:11 +0900

    Cleanup, fixed item pickup, added terrible animation

--- Assets/Gordon/Chef.gd
+++ Assets/Gordon/Chef.gd
@@ -4,10 +4,9 @@ var velocity = Vector2(0, 0)
 
 export var speed = 32
 export var gravity = 30
-# May want to use position 2Ds in the future
 # This garbage code is just for testing
-export var target_position = 50 
-var player = null
+export var target_position = 200
+onready var animationPlayer = $AnimationPlayer
 
 func _ready():
 	pass
@@ -18,12 +17,13 @@ func _process(delta):
 func move_character():
 	if ($".".position.x < target_position - 1):
 		velocity.x = speed * 1
-		$Sprite.flip_h = false
+		animationPlayer.play("Walk Right")
 	elif ($".".position.x > target_position + 1):
 		velocity.x = speed * -1
-		$Sprite.flip_h = true
+		animationPlayer.play("Walk Left")
 	else:
 		velocity.x = 0
+		animationPlayer.play("Idle Right")
 
 	velocity.y = gravity
 
