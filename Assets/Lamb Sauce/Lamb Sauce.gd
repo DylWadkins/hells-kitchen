92bc714b6e63dc6d0f684ba468456bef9f61c84e
Dylan Wadkins <dylantwadkins@gmail.com>
2022-07-12 18:20:21 -0400

    Fixed laaamb boat

--- Assets/Lamb Sauce/Lamb Sauce.gd
+++ Assets/Lamb Sauce/Lamb Sauce.gd
@@ -4,14 +4,20 @@ extends Area2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-func _on_Area2D_body_entered(body):
+func _on_Lamb_Sauce_body_entered(body):
 	print("Entered lamb sauce")
 	if (body.lambSauce != null):
 		print("Changing lamb sauce variable on player")
 		body.lambSauce = true
 		$Slurp.play(0.4)
-		queue_free()
+		$Collision.queue_free()
+		$Sprite.visible = false
+		$Timer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Timer_timeout():
+	queue_free()
